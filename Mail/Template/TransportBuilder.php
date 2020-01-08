4f6185e6c5d55e48dbf810d41ba525938384f54c
Sander Hartman <sander@weprovide.com>
2020-01-08 08:20:31 +0100

    Remove unneeded properties

--- Mail/Template/TransportBuilder.php
+++ Mail/Template/TransportBuilder.php
@@ -1,31 +1,25 @@
 <?php
-/**
- * Mail Template Transport Builder
- *
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
 declare(strict_types=1);
 
-namespace Magento\Framework\Mail\Template;
+namespace WeProvide\MailAttachment\Mail\Template;
 
 use Magento\Framework\App\TemplateTypesInterface;
 use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Exception\MailException;
-use Magento\Framework\Mail\EmailMessageInterface;
-use Magento\Framework\Mail\EmailMessageInterfaceFactory;
 use Magento\Framework\Mail\AddressConverter;
+use Magento\Framework\Mail\EmailMessageInterfaceFactory;
 use Magento\Framework\Mail\Exception\InvalidArgumentException;
 use Magento\Framework\Mail\MessageInterface;
 use Magento\Framework\Mail\MessageInterfaceFactory;
 use Magento\Framework\Mail\MimeInterface;
 use Magento\Framework\Mail\MimeMessageInterfaceFactory;
 use Magento\Framework\Mail\MimePartInterfaceFactory;
-use Magento\Framework\Mail\TemplateInterface;
-use Magento\Framework\Mail\TransportInterface;
+use Magento\Framework\Mail\Template\FactoryInterface;
+use Magento\Framework\Mail\Template\SenderResolverInterface;
 use Magento\Framework\Mail\TransportInterfaceFactory;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\Framework\Phrase;
+use Zend\Mime\Mime;
+use Zend\Mime\PartFactory;
 
 /**
  * TransportBuilder
@@ -34,106 +28,30 @@ use Magento\Framework\Phrase;
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  * @since 100.0.2
  */
-class TransportBuilder
+class TransportBuilder extends \Magento\Framework\Mail\Template\TransportBuilder
 {
-    /**
-     * Template Identifier
-     *
-     * @var string
-     */
-    protected $templateIdentifier;
+    /** @var array */
+    protected $attachments = [];
 
-    /**
-     * Template Model
-     *
-     * @var string
-     */
-    protected $templateModel;
+    /** @var mixed */
+    protected $partFactory;
 
-    /**
-     * Template Variables
-     *
-     * @var array
-     */
-    protected $templateVars;
-
-    /**
-     * Template Options
-     *
-     * @var array
-     */
-    protected $templateOptions;
-
-    /**
-     * Mail Transport
-     *
-     * @var TransportInterface
-     */
-    protected $transport;
-
-    /**
-     * Template Factory
-     *
-     * @var FactoryInterface
-     */
-    protected $templateFactory;
-
-    /**
-     * Object Manager
-     *
-     * @var ObjectManagerInterface
-     */
-    protected $objectManager;
-
-    /**
-     * Message
-     *
-     * @var EmailMessageInterface
-     */
-    protected $message;
-
-    /**
-     * Sender resolver
-     *
-     * @var SenderResolverInterface
-     */
-    protected $_senderResolver;
-
-    /**
-     * @var TransportInterfaceFactory
-     */
-    protected $mailTransportFactory;
-
-    /**
-     * Param that used for storing all message data until it will be used
-     *
-     * @var array
-     */
+    /** @var array */
     private $messageData = [];
 
-    /**
-     * @var EmailMessageInterfaceFactory
-     */
+    /** @var EmailMessageInterfaceFactory */
     private $emailMessageInterfaceFactory;
 
-    /**
-     * @var MimeMessageInterfaceFactory
-     */
+    /** @var MimeMessageInterfaceFactory */
     private $mimeMessageInterfaceFactory;
 
-    /**
-     * @var MimePartInterfaceFactory
-     */
+    /** @var MimePartInterfaceFactory */
     private $mimePartInterfaceFactory;
 
-    /**
-     * @var AddressConverter|null
-     */
+    /** @var AddressConverter|null */
     private $addressConverter;
 
     /**
-     * TransportBuilder constructor
-     *
      * @param FactoryInterface $templateFactory
      * @param MessageInterface $message
      * @param SenderResolverInterface $senderResolver
@@ -159,188 +77,16 @@ class TransportBuilder
         MimePartInterfaceFactory $mimePartInterfaceFactory = null,
         AddressConverter $addressConverter = null
     ) {
-        $this->templateFactory = $templateFactory;
-        $this->objectManager = $objectManager;
-        $this->_senderResolver = $senderResolver;
-        $this->mailTransportFactory = $mailTransportFactory;
-        $this->emailMessageInterfaceFactory = $emailMessageInterfaceFactory ?: $this->objectManager
+        parent::__construct($templateFactory, $message, $senderResolver, $objectManager, $mailTransportFactory, $messageFactory, $emailMessageInterfaceFactory, $mimeMessageInterfaceFactory, $mimePartInterfaceFactory, $addressConverter);
+        $this->emailMessageInterfaceFactory = $emailMessageInterfaceFactory ?: $objectManager
             ->get(EmailMessageInterfaceFactory::class);
-        $this->mimeMessageInterfaceFactory = $mimeMessageInterfaceFactory ?: $this->objectManager
+        $this->mimeMessageInterfaceFactory = $mimeMessageInterfaceFactory ?: $objectManager
             ->get(MimeMessageInterfaceFactory::class);
-        $this->mimePartInterfaceFactory = $mimePartInterfaceFactory ?: $this->objectManager
+        $this->mimePartInterfaceFactory = $mimePartInterfaceFactory ?: $objectManager
             ->get(MimePartInterfaceFactory::class);
-        $this->addressConverter = $addressConverter ?: $this->objectManager
+        $this->addressConverter = $addressConverter ?: $objectManager
             ->get(AddressConverter::class);
-    }
-
-    /**
-     * Add cc address
-     *
-     * @param array|string $address
-     * @param string $name
-     *
-     * @return $this
-     */
-    public function addCc($address, $name = '')
-    {
-        $this->addAddressByType('cc', $address, $name);
-
-        return $this;
-    }
-
-    /**
-     * Add to address
-     *
-     * @param array|string $address
-     * @param string $name
-     *
-     * @return $this
-     * @throws InvalidArgumentException
-     */
-    public function addTo($address, $name = '')
-    {
-        $this->addAddressByType('to', $address, $name);
-
-        return $this;
-    }
-
-    /**
-     * Add bcc address
-     *
-     * @param array|string $address
-     *
-     * @return $this
-     * @throws InvalidArgumentException
-     */
-    public function addBcc($address)
-    {
-        $this->addAddressByType('bcc', $address);
-
-        return $this;
-    }
-
-    /**
-     * Set Reply-To Header
-     *
-     * @param string $email
-     * @param string|null $name
-     *
-     * @return $this
-     * @throws InvalidArgumentException
-     */
-    public function setReplyTo($email, $name = null)
-    {
-        $this->addAddressByType('replyTo', $email, $name);
-
-        return $this;
-    }
-
-    /**
-     * Set mail from address
-     *
-     * @param string|array $from
-     *
-     * @return $this
-     * @throws InvalidArgumentException
-     * @see setFromByScope()
-     *
-     * @deprecated 102.0.1 This function sets the from address but does not provide
-     * a way of setting the correct from addresses based on the scope.
-     */
-    public function setFrom($from)
-    {
-        return $this->setFromByScope($from);
-    }
-
-    /**
-     * Set mail from address by scopeId
-     *
-     * @param string|array $from
-     * @param string|int $scopeId
-     *
-     * @return $this
-     * @throws InvalidArgumentException
-     * @throws MailException
-     * @since 102.0.1
-     */
-    public function setFromByScope($from, $scopeId = null)
-    {
-        $result = $this->_senderResolver->resolve($from, $scopeId);
-        $this->addAddressByType('from', $result['email'], $result['name']);
-
-        return $this;
-    }
-
-    /**
-     * Set template identifier
-     *
-     * @param string $templateIdentifier
-     *
-     * @return $this
-     */
-    public function setTemplateIdentifier($templateIdentifier)
-    {
-        $this->templateIdentifier = $templateIdentifier;
-
-        return $this;
-    }
-
-    /**
-     * Set template model
-     *
-     * @param string $templateModel
-     *
-     * @return $this
-     */
-    public function setTemplateModel($templateModel)
-    {
-        $this->templateModel = $templateModel;
-        return $this;
-    }
-
-    /**
-     * Set template vars
-     *
-     * @param array $templateVars
-     *
-     * @return $this
-     */
-    public function setTemplateVars($templateVars)
-    {
-        $this->templateVars = $templateVars;
-
-        return $this;
-    }
-
-    /**
-     * Set template options
-     *
-     * @param array $templateOptions
-     * @return $this
-     */
-    public function setTemplateOptions($templateOptions)
-    {
-        $this->templateOptions = $templateOptions;
-
-        return $this;
-    }
-
-    /**
-     * Get mail transport
-     *
-     * @return TransportInterface
-     * @throws LocalizedException
-     */
-    public function getTransport()
-    {
-        try {
-            $this->prepareMessage();
-            $mailTransport = $this->mailTransportFactory->create(['message' => clone $this->message]);
-        } finally {
-            $this->reset();
-        }
-
-        return $mailTransport;
+        $this->partFactory = $objectManager->get(PartFactory::class);
     }
 
     /**
@@ -354,19 +100,8 @@ class TransportBuilder
         $this->templateIdentifier = null;
         $this->templateVars = null;
         $this->templateOptions = null;
+        $this->attachments = [];
         return $this;
-    }
-
-    /**
-     * Get template
-     *
-     * @return TemplateInterface
-     */
-    protected function getTemplate()
-    {
-        return $this->templateFactory->get($this->templateIdentifier, $this->templateModel)
-            ->setVars($this->templateVars)
-            ->setOptions($this->templateOptions);
     }
 
     /**
@@ -394,8 +129,9 @@ class TransportBuilder
                 );
         }
         $mimePart = $this->mimePartInterfaceFactory->create(['content' => $content]);
+        $parts = count($this->attachments) ? array_merge([$mimePart], $this->attachments) : [$mimePart];
         $this->messageData['body'] = $this->mimeMessageInterfaceFactory->create(
-            ['parts' => [$mimePart]]
+            ['parts' => $parts]
         );
 
         $this->messageData['subject'] = html_entity_decode(
@@ -430,5 +166,24 @@ class TransportBuilder
                 $convertedAddressArray
             );
         }
+    }
+
+    /**
+     * @param string|null $content
+     * @param string|null $fileName
+     * @param string|null $fileType
+     * @return TransportBuilder
+     */
+    public function addAttachment(?string $content, ?string $fileName, ?string $fileType)
+    {
+        $attachmentPart = $this->partFactory->create();
+        $attachmentPart->setContent($content)
+            ->setType($fileType)
+            ->setFileName($fileName)
+            ->setDisposition(Mime::DISPOSITION_ATTACHMENT)
+            ->setEncoding(Mime::ENCODING_BASE64);
+        $this->attachments[] = $attachmentPart;
+
+        return $this;
     }
 }
